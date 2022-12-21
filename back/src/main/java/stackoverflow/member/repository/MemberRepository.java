8d5d675d104d0134b39ba3e3f5caca6fd0a13436
AAmorning <ygang8549@gmail.com>
2022-12-21 19:37:38 +0900

    [feat] Member repository 추가

---
+++ back/src/main/java/stackoverflow/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package stackoverflow.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import stackoverflow.member.entity.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
+
