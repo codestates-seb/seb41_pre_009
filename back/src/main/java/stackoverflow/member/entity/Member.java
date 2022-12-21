87b75ec5c0981c1d90d72eaf0f6ba25c8ba541a3
AAmorning <ygang8549@gmail.com>
2022-12-21 19:22:39 +0900

    [feat] Member entity 추가

--- back/src/main/java/stackoverflow/member/entity/Member.java
+++ back/src/main/java/stackoverflow/member/entity/Member.java
@@ -1,2 +1,43 @@
-package stackoverflow.member.entity;public class Member {
+package stackoverflow.member.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import stackoverflow.audit.Auditable;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String passWord;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_SLEEP("휴면 상태"),
+        MEMBER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
 }
