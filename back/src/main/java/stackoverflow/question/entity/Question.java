55397dc7ab4be06197f6e2042dd04830808e8c9e
AAmorning <111403585+AAmorning@users.noreply.github.com>
2022-12-23 14:10:59 +0900

    [feat] JPA entity mapping 생성 (#23)
    * [Feat] Member, Answer, Question, Comment 도메인 구현 (#18)
    
     [Feat] Answer 엔티티 추가
    
     [Feat] Answer Repository 추가
    
     [Feat] AnswerDto 추가
    
    * [feat] Member entity 추가
    
    * [feat] Member repository 추가
    
    * [feat] Member dto 추가
    
    * [feat] Member mapper 추가
    
    * [feat] Member service 추가
    
    * [feat] Member controller 추가
    
    * [Feat] Question Entity 구현
    
    * [Feat] Question Repository 구현
    
    * [Feat] Question Dto 구현
    
    * [Feat] Question Mapper 구현
    
    * [Feat] Question Controller 구현
    
    * [Feat] Question Service 구현
    
    * [Feat] Answer 엔티티 추가
    
    * [Feat] Answer Repository 추가
    
    * [Feat] AnswerDto 추가
    
    * [Feat] AnswerMapper 추가
    
    * [Feat] AnswerService 추가
    
    * [Feat] AnswerController 추가
    
    * [Feat] Comment 엔티티 추가
    
    * [Feat] CommentRepository 추가
    
    * [Feat] CommentDto 추가
    
    * [Feat] CommentMapper 추가
    
    * [Feat] CommentService 추가
    
    * [Feat] CommentController 추가
    
    * [feat] JPA entity mapping 생성
    
    Co-authored-by: Kangharyeom <108250233+kangharyeom@users.noreply.github.com>

--- back/src/main/java/stackoverflow/question/entity/Question.java
+++ back/src/main/java/stackoverflow/question/entity/Question.java
@@ -3,10 +3,16 @@ package stackoverflow.question.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import stackoverflow.answer.entity.Answer;
 import stackoverflow.audit.Auditable;
+import stackoverflow.member.entity.Member;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @NoArgsConstructor
 @Getter
@@ -34,6 +40,17 @@ public class Question extends Auditable{
 
     @Column
     private Integer view; //null 허용 여부 논의하기
+
+
+    @OneToMany(mappedBy = "question")
+    List<Answer> answers = new ArrayList<>();
+
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+
 
     public Question(String title, String content, Integer view) {
         this.title = title;
